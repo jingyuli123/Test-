6b878fd06506a3b6631b006ef756f11578dcace9
18743007098@139.com <18743007098@139.com>
2016-03-05 11:13:13 +0800

    对象重写HashCode方法

---
+++ SetTest3.java
@@ -0,0 +1,45 @@
+import java.util.HashSet;
+
+public class SetTest3 {
+	
+	public static void main(String[] args) {
+		
+		HashSet set = new HashSet();
+		
+		Student s1 = new Student("lisi");
+		Student s2 = new Student("lisi");
+		
+		set.add(s1);
+		set.add(s2);
+		
+		System.out.println(set);
+	}
+	
+}
+
+class Student {
+	
+	String name;
+	
+	public Student(String name) {
+		this.name = name;
+	}
+	
+	public int hashCode() {
+		return this.name.hashCode();
+	}
+	
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (null != obj && obj instanceof Student) {
+			Student s = (Student)obj;
+			if (name.equals(s.name)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
