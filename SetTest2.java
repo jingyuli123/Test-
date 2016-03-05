cf170fa41ab14e9929ac9b0f5b76bb4e39675397
18743007098@139.com <18743007098@139.com>
2016-03-05 10:49:34 +0800

    HashSet添加对象

---
+++ SetTest2.java
@@ -0,0 +1,46 @@
+import java.util.HashSet;
+
+public class SetTest2 {
+	
+	public static void main(String[] args) {
+		
+		HashSet set = new HashSet();
+		
+		//set.add(new People("张三"));
+		//set.add(new People("lisi"));
+		//set.add(new People("万物"));
+		
+		//System.out.println(set);
+		//[People@55f33675, People@65690726, People@527c6768]
+		
+		//People p1 = new People("zhangsan");
+		
+		//set.add(p1);
+		//set.add(p1);
+		
+		//System.out.println(set);//[People@459189e1]
+		
+		String s1 = new String("a");
+		String s2 = new String("a");
+		
+		System.out.println(s1.hashCode() == s2.hashCode());//true
+		
+		set.add(s1);
+		set.add(s2);
+		
+		System.out.println(set);//[a]
+		
+		
+	}
+	
+}
+
+class People {
+	
+	String name;
+	
+	public People(String name) {
+		this.name = name;
+	}
+	
+}
