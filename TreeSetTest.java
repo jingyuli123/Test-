23a6171326a7234e74dca714c754a4c86452d53e
18743007098@139.com <18743007098@139.com>
2016-03-06 20:18:07 +0800

    TreeSet按升序输出

---
+++ TreeSetTest.java
@@ -0,0 +1,21 @@
+import java.util.TreeSet;
+
+public class TreeSetTest {
+	
+	public static void main(String[] args) {
+		
+		TreeSet set = new TreeSet();
+		
+		set.add("c");
+		set.add("d");
+		set.add("b");
+		set.add("c");
+		set.add("a");
+		
+		System.out.println(set);//[a, b, c, d]
+		
+		
+	}
+	
+}
+
