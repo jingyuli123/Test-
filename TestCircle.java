8ec2d218b98337e4db290d02c87f3072e20bbde2
18743007098@139.com <18743007098@139.com>
2016-03-22 09:09:06 +0800

    接口应用的练习

---
+++ TestCircle.java
@@ -0,0 +1,62 @@
+
+
+public class TestCircle {
+	public static void main(String[] args) {
+		ComparableCircle c1 = new ComparableCircle(2.3);
+		ComparableCircle c2 = new ComparableCircle(2.1);
+		ComparableCircle c3 = new ComparableCircle(2.3);
+		
+		int i1 = c1.compareTo(c2);
+		int i2 = c1.compareTo(c3);
+		int i3 = c2.compareTo(c1);
+		
+		System.out.println(i1);
+		System.out.println(i2);
+		System.out.println(i3);
+	}
+}
+
+interface CompareObject {
+	int compareTo(Object o);
+}
+
+class Circle {
+	private double  radius;
+	
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+	
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+}
+
+class ComparableCircle extends Circle implements CompareObject {
+	
+	public ComparableCircle(double radius) {
+		super(radius);
+	}
+	
+	@Override
+	public int compareTo(Object o) {
+		if(this == o) {
+			return 0;
+		}else if(o instanceof ComparableCircle) {
+			ComparableCircle c = (ComparableCircle)o;
+			if (this.getRadius() > c.getRadius()) {
+				return 1;
+			}else if (this.getRadius() < c.getRadius()) {
+				return -1;
+			}else {
+				return 0;
+			}
+		}else {
+			throw new RuntimeException("传入的非ComparebleCircle的对象，不可以进行比较");
+		}
+	}
+}
